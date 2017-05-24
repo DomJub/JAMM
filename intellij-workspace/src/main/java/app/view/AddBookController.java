617a8cd329a76b7ad2e64b2fdd86f69d8623b194
DomJub <dburnand@yahoo.fr>
2017-05-24 15:34:35 +0200

    ajout fenetre creation differentes oeuvres

---
+++ intellij-workspace/src/main/java/app/view/AddBookController.java
@@ -0,0 +1,7 @@
+package app.view;
+
+/**
+ * Created by Jub on 24/05/2017.
+ */
+public class AddBookController {
+}
--- intellij-workspace/src/main/java/app/view/Controller.java
+++ intellij-workspace/src/main/java/app/view/Controller.java
@@ -1,16 +1,22 @@
 package app.view;
 
+import java.io.IOException;
 import java.net.URL;
 
 
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -18,6 +24,7 @@ import javafx.scene.control.TableView;
 import app.model.Oeuvre;
 import app.repository.OeuvreRepository;
 import app.repository.impl.OeuvreRepositoyImpl;
+import javafx.stage.Stage;
 
 public class Controller implements Initializable {
     @FXML
@@ -51,7 +58,35 @@ public class Controller implements Initializable {
     }
 
 
-    public void openAddBookView(ActionEvent actionEvent) {
-
+    public void openAddBookView(ActionEvent actionEvent) throws IOException{
+        changePage("/AddBookView.fxml");
     }
+
+    public void openAddAudioView(ActionEvent actionEvent) {
+        changePage("/AddAudioView.fxml");
+    }
+
+    public void openAddMovieView(ActionEvent actionEvent) {
+        changePage("/AddMovieView.fxml");
+    }
+
+    public void openAddVideoGameView(ActionEvent actionEvent) {
+        changePage("/AddVideoGameView.fxml");
+    }
+
+    private void changePage(String location) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(location));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(IOException e) {
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.log(Level.SEVERE, "Failed to create new Window.", e);
+        }
+    }
+
+
+
 }
