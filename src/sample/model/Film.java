6195c5251ab717246447ee6b7cf9c46ca165a866
lav <pl_dtm@hotmail.com>
2017-05-24 11:47:33 +0200

    restructuration format maven

--- src/sample/model/Film.java
+++
@@ -1,8 +0,0 @@
-package sample.model;
-
-/**
- * Created by Jub on 12/05/2017.
- */
-public class Film extends Oeuvre {
-
-}
--- src/sample/model/Livre.java
+++
@@ -1,8 +0,0 @@
-package sample.model;
-
-/**
- * Created by Jub on 12/05/2017.
- */
-public class Livre extends Oeuvre {
-
-}
