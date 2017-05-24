82e402cf40415da6b94099d0405ed681b5abc9d1
DomJub <dburnand@yahoo.fr>
2017-05-24 12:04:53 +0200

    modif repertoire resources maven

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("view/MainView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/MainView.fxml"));
         primaryStage.setTitle("JAMM - Gestion Mediathèque");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
