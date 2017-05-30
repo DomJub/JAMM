30e5502d8c9ae1c7cebedc03e39eb5c1a2787312
lav <pl_dtm@hotmail.com>
2017-05-30 13:51:04 +0200

    ajout action popup

--- intellij-workspace/src/main/java/app/repository/AbstractConnect.java
+++ intellij-workspace/src/main/java/app/repository/AbstractConnect.java
@@ -9,43 +9,42 @@ import java.text.SimpleDateFormat;
  */
 public abstract class AbstractConnect {
 
-	public static final String COM_MYSQL_JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	private final static String user = "dbuser";
-	private final static String psswd = "dbuser";
-	private static String dbname = "jamm";
-	private static String host = "10.1.2.157:3306";
+    public static final String COM_MYSQL_JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private final static String user = "dbuser";
+    private final static String psswd = "dbuser";
+    static Connection conn;
+    private static String dbname = "jamm";
 //	private final static String user = "root";
 //	private final static String psswd = "root";
 //	private static String dbname = "jamm";
 //	private static String host = "127.0.0.1";
+    private static String host = "10.1.2.157:3306";
 
-	static Connection conn;
+    public AbstractConnect() throws ClassNotFoundException, SQLException {
+        try {
+            Class.forName(COM_MYSQL_JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            log(e.getMessage());
+        }
+    }
 
-	public AbstractConnect() throws ClassNotFoundException, SQLException {
-		try {
-			Class.forName(COM_MYSQL_JDBC_DRIVER);
-		} catch (ClassNotFoundException e) {
-			log(e.getMessage());
-		}
-	}
+    public static Connection getConnection() throws SQLException {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://" + host + "/" + dbname, user, psswd);
+        } catch (SQLException e) {
+            log("Connexion failed!");
+            e.printStackTrace();
+        }
+        return conn;
+    }
 
-	public static Connection getConnection() throws SQLException {
-		try {
-			conn = DriverManager.getConnection("jdbc:mysql://" + host + "/" + dbname, user, psswd);
-		} catch (SQLException e) {
-			log("Connexion failed!");
-			e.printStackTrace();
-		}
-		return conn;
-	}
+    public static void log(String message) {
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS");
+        System.out.println("[" + formatter.format(new Timestamp(new java.util.Date().getTime())) + "] : " + message);
+    }
 
-	public void closeConnection() throws SQLException {
-		conn.close();
-	}
-
-	public static void log(String message) {
-		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS");
-		System.out.println("[" + formatter.format(new Timestamp(new java.util.Date().getTime())) + "] : " + message);
-	}
+    public void closeConnection() throws SQLException {
+        conn.close();
+    }
 
 }
