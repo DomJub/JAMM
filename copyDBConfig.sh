15716089ce0a5af33b0cc54c40ece34481e3515d
yahyalexandre <ralie.moter@gmail.com>
2017-05-30 12:43:58 +0000

    Test

--- copyDBConfig.sh
+++ copyDBConfig.sh
@@ -1,3 +1,4 @@
+echo "test"
 echo "begin copyDBConfig.sh script"
 cp DBConfig.java /home/travis/build/abou-imran/GED/intellij-workspace/src/main/java/app/repository/DBConfig.java -v
 ls /home/travis/build/abou-imran/GED/intellij-workspace/src/main/java/app/repository/
