e40ba34d6f1049a520a86094d7cb3ec3deb2f4db
yahyalexandre <ralie.moter@gmail.com>
2017-05-30 12:32:00 +0000

    Test copyDBConfig

--- copyDBConfig.sh
+++ copyDBConfig.sh
@@ -1,1 +1,4 @@
-cp DBConfig.java intellij-workspace/src/main/java/app/repository/DBConfig.java
+echo "begin copyDBConfig.sh script"
+cp DBConfig.java /home/travis/build/abou-imran/GED/intellij-workspace/src/main/java/app/repository/DBConfig.java -v
+ls /home/travis/build/abou-imran/GED/intellij-workspace/src/main/java/app/repository/
+echo "End copyDBConfig.sh script"
