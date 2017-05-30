e87212f87ba49b50c5a0440c5e421ae1162c8bef
yahyalexandre <ralie.moter@gmail.com>
2017-05-30 13:58:35 +0000

    Proprification [ci skip]

--- copyDBConfig.sh
+++
@@ -1,5 +0,0 @@
-echo "test"
-echo "begin copyDBConfig.sh script"
-cp DBConfig.java /home/travis/build/abou-imran/GED/intellij-workspace/src/main/java/app/repository/DBConfig.java -v
-ls /home/travis/build/abou-imran/GED/intellij-workspace/src/main/java/app/repository/
-echo "End copyDBConfig.sh script"
