28a30466b4fab77e9ec7319a564954c56aac2698
DomJub <dburnand@yahoo.fr>
2017-06-02 11:30:47 +0200

    Implementation ajout piste musicale

--- intellij-workspace/src/main/java/app/model/CD.java
+++ intellij-workspace/src/main/java/app/model/CD.java
@@ -1,27 +1,31 @@
 package app.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by Jub on 12/05/2017.
  */
 public class CD extends Oeuvre {
 
-    public String getListe_piste() {
-        return liste_piste;
+    protected ArrayList<PisteMusicale> listePiste ;
+
+    public ArrayList<PisteMusicale> getListePiste() {
+        return this.listePiste;
     }
 
-    public void setListe_piste(String liste_piste) {
-        this.liste_piste = liste_piste;
+    public void setListePiste(ArrayList<PisteMusicale> listePiste) {
+        this.listePiste = listePiste;
     }
 
-    protected String liste_piste;
 
-    public CD(String nom, String auteur, String libelle_auteur, String origine_acquisition, String support, int note, String commentaire, String liste_piste) {
+
+    public CD(String nom, String auteur, String libelle_auteur, String origine_acquisition, String support, int note, String commentaire, ArrayList<PisteMusicale> listePiste) {
         super(nom, auteur, libelle_auteur, origine_acquisition, support, note, commentaire);
-        this.liste_piste = liste_piste;
+        this.listePiste = listePiste;
     }
 
     public CD() {
         super();
-        this.liste_piste = "";
+        this.listePiste = new ArrayList<PisteMusicale>();
     }
 }
